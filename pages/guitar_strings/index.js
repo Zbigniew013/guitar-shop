81e922c34e27610a692df5ff2eea387dfbd4164f
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-07-01 20:51:14 +0200

    fix_3: build page for all strings

--- pages/guitar_strings/index.js
+++ pages/guitar_strings/index.js
@@ -2,11 +2,14 @@ import Image from 'next/image';
 import { GraphQLClient, gql } from 'graphql-request';
 import Link from 'next/link';
 
-const hygraph = new GraphQLClient(process.env.HYGRAPH_API_READONLY_TOKEN, {
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
-  },
-});
+const hygraph = new GraphQLClient(
+  'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/cli78silh3hv001t7flkz4kx7/master',
+  {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
+    },
+  }
+);
 
 const GuitarStrings = gql`
   {
