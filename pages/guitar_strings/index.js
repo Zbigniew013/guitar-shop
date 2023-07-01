cd443e8ca0aeb8ca59e78a9bef3042e8505a36e4
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-07-01 20:47:15 +0200

    fix_2: build page for all strings

--- pages/guitar_strings/index.js
+++ pages/guitar_strings/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import { GraphQLClient, gql } from 'graphql-request';
 import Link from 'next/link';
 
-const hygraph = new GraphQLClient(process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT, {
+const hygraph = new GraphQLClient(process.env.HYGRAPH_API_READONLY_TOKEN, {
   headers: {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
   },
