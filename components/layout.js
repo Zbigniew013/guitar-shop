201ac6f78519a72b2eb64e599eee385ba3065fa1
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-07-06 12:27:30 +0200

    fix:adding pagination to Strings page

--- components/layout.js
+++ components/layout.js
@@ -5,14 +5,14 @@ function Layout(props) {
   return (
     <>
       <div className='flex flex-col min-h-screen'>
-        <div className='flex flex-col h-screen'>
-          <div className='flex flex-col flex-grow'>
-            <Header />
-            {props.children}
-            <Footer />
-          </div>
-        </div>
+        {/* <div className='flex flex-col h-screen'> */}
+        {/* <div className='flex flex-col flex-grow'> */}
+        <Header />
+        {props.children}
+        <Footer />
       </div>
+      {/* </div> */}
+      {/* </div> */}
     </>
   );
 }
