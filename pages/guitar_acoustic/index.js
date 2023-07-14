77173b4661ca19b6ac4f8d542a6522002c18a7f9
Zbigniew Szalkowski <zbigniew.szalkowski@gmail.com>
2023-07-14 22:14:44 +0200

    fix: adding image modal to singleStrings page

--- pages/guitar_acoustic/index.js
+++ pages/guitar_acoustic/index.js
@@ -1,14 +1,14 @@
-// function AcousticGuitarPage() {
-//   return (
-//     <div className='flex flex-col flex-1 max-w-5xl w-full mx-auto bg-stone-100'>
-//       <h1 className='flex-grow text-3xl font-bold text-center underline text-red-400 '>
-//         The Acoustic Guitars Page
-//       </h1>
-//     </div>
-//   );
-// }
+function AcousticGuitarPage() {
+  return (
+    <div className='flex flex-col flex-1 max-w-5xl w-full mx-auto bg-stone-100'>
+      <h1 className='flex-grow text-3xl font-bold text-center underline text-red-400 '>
+        The Acoustic Guitars Page
+      </h1>
+    </div>
+  );
+}
 
-// export default AcousticGuitarPage;
+export default AcousticGuitarPage;
 
 // import Link from 'next/link';
 // import { GraphQLClient } from 'graphql-request';
@@ -45,57 +45,3 @@
 //     </Link>
 //   ));
 // };
-
-import React from 'react';
-
-export default function Modal() {
-  const [showModal, setShowModal] = React.useState(false);
-  return (
-    <>
-      <button
-        className='bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-        type='button'
-        onClick={() => setShowModal(true)}
-      >
-        Open large modal
-      </button>
-      {showModal ? (
-        <>
-          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-            <div className='relative w-auto my-6 mx-auto max-w-6xl'>
-              {/*content*/}
-              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
-                <button
-                  className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
-                  onClick={() => setShowModal(false)}
-                >
-                  <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
-                    ×
-                  </span>
-                </button>
-                {/*body*/}
-                <div className='relative p-6 flex-auto'>
-                  <p className='my-4 text-slate-500 text-lg leading-relaxed'>
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
-                  </p>
-                </div>
-                <button
-                  className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                  type='button'
-                  onClick={() => setShowModal(false)}
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
-        </>
-      ) : null}
-    </>
-  );
-}
--- pages/guitar_electric/index.js
+++ pages/guitar_electric/index.js
@@ -32,50 +32,50 @@
 //     </Link>
 //   ));
 
-// function ElectricGuitarPage() {
-//   return (
-//     <div className='flex flex-col relative flex-1 max-w-5xl w-full mx-auto bg-stone-100'>
-//       <h1 className=' text-3xl font-bold text-center underline text-red-400 '>
-//         The Electric Guitars Page
-//       </h1>
-//     </div>
-//   );
-// }
-
-// export default ElectricGuitarPage;
-
-import Link from 'next/link';
-import { GraphQLClient } from 'graphql-request';
-
-export async function getStaticProps() {
-  const hygraph = new GraphQLClient(
-    'https://api-eu-central-1.hygraph.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master'
-    // 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cli78silh3hv001t7flkz4kx7/master'
+function ElectricGuitarPage() {
+  return (
+    <div className='flex flex-col relative flex-1 max-w-5xl w-full mx-auto bg-stone-100'>
+      <h1 className=' text-3xl font-bold text-center underline text-red-400 '>
+        The Electric Guitars Page
+      </h1>
+    </div>
   );
-
-  const { products } = await hygraph.request(
-    `
-      {
-        products {
-          slug
-          name
-        }
-      }
-    `
-  );
-
-  return {
-    props: {
-      products,
-    },
-  };
 }
 
-const IndexPage = ({ products }) =>
-  products.map(({ slug, name }) => (
-    <Link key={slug} href={`/guitar_electric/${slug}`}>
-      <div>{name}</div>
-    </Link>
-  ));
+export default ElectricGuitarPage;
 
-export default IndexPage;
+// import Link from 'next/link';
+// import { GraphQLClient } from 'graphql-request';
+
+// export async function getStaticProps() {
+//   const hygraph = new GraphQLClient(
+//     'https://api-eu-central-1.hygraph.com/v2/ck8sn5tnf01gc01z89dbc7s0o/master'
+//     // 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cli78silh3hv001t7flkz4kx7/master'
+//   );
+
+//   const { products } = await hygraph.request(
+//     `
+//       {
+//         products {
+//           slug
+//           name
+//         }
+//       }
+//     `
+//   );
+
+//   return {
+//     props: {
+//       products,
+//     },
+//   };
+// }
+
+// const IndexPage = ({ products }) =>
+//   products.map(({ slug, name }) => (
+//     <Link key={slug} href={`/guitar_electric/${slug}`}>
+//       <div>{name}</div>
+//     </Link>
+//   ));
+
+// export default IndexPage;
